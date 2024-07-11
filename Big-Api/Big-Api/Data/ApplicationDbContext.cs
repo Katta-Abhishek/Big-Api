58796207e8947b9d0f100f77184451478f9dbd7d
Abhishek Katta <abhishek@samisen.io>
2024-07-11 21:30:04 +0530

    implemented api project

---
+++ Big-Api/Big-Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Big_Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Big_Api.Data;
+
+public class ApplicationDbContext : DbContext
+{
+  public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+  public DbSet<AppEntity> appEntities { get; set; }
+}
---
+++ Big-Api/Big-Api/Models/AppEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Big_Api.Models;
+
+public class AppEntity
+{
+  public int Id { get; set; }
+  public string Name { get; set; } = string.Empty;
+}
